d182b610227f760aba0d5c8b4466eb233f2f8e43
leandro.lomb <leandro.lombi@synchro.com.br>
2021-08-05 09:41:36 -0300

    Estruturando o projeto

---
+++ contas/contacorrente.go
@@ -0,0 +1,43 @@
+package contas
+
+// Estrutura da conta corrente do banco
+type ContaCorrente struct {
+	Titular string
+	NumeroAgencia int
+	NumeroConta int
+	Saldo float64
+}
+
+func (c *ContaCorrente) Sacar(valor float64) string {
+	if valor < 0 {
+		return "Não é possível sacar um valor negativo"
+	}
+
+	podeSacar := valor <= c.Saldo
+
+ 	if podeSacar {
+		c.Saldo -= valor
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
+func (c *ContaCorrente) Depositar(valor float64) (string, float64) {
+	if valor > 0 {
+		c.Saldo += valor
+		return "Deposito realizado com sucesso. Saldo:", c.Saldo
+	} else {
+		return "O valor do deposito não pode ser menor que zero. Saldo:", c.Saldo
+	}
+}
+
+func (c *ContaCorrente) Transferir(valor float64, contaDestino *ContaCorrente) bool {
+	if valor <= c.Saldo && valor > 0 {
+		c.Saldo -= valor
+		contaDestino.Depositar(valor)
+		return true
+	} else {
+		return false
+	}
+}
--- main.go
+++ main.go
@@ -1,64 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-// Estrutura da conta corrente do banco
-type ContaCorrente struct {
-	titular string
-	numeroAgencia int
-	numeroConta int
-	saldo float64
-}
-
-func (c *ContaCorrente) Sacar(valor float64) string {
-	if valor < 0 {
-		return "Não é possível sacar um valor negativo"
-	}
-
-	podeSacar := valor <= c.saldo
-
- 	if podeSacar {
-		c.saldo -= valor
-		return "Saque realizado com sucesso"
-	} else {
-		return "Saldo insuficiente"
-	}
-}
-
-func (c *ContaCorrente) Depositar(valor float64) (string, float64) {
-	if valor > 0 {
-		c.saldo += valor
-		return "Deposito realizado com sucesso. Saldo:", c.saldo
-	} else {
-		return "O valor do deposito não pode ser menor que zero. Saldo:", c.saldo
-	}
-}
-
-func (c *ContaCorrente) Transferir(valor float64, contaDestino *ContaCorrente) bool {
-	if valor <= c.saldo && valor > 0 {
-		c.saldo -= valor
-		contaDestino.Depositar(valor)
-		return true
-	} else {
-		return false
-	}
-}
+	"github.com/lnl/banco/contas"
+)
 
 func main()  {
-	contaLeandro := ContaCorrente{
-		titular: "Leandro",
-		numeroConta: 123654,
-		saldo: 102.6,
+	contaLeandro := contas.ContaCorrente{
+		Titular: "Leandro",
+		NumeroConta: 123654,
+		Saldo: 102.6,
 	}
 
-	contaLombi := ContaCorrente {
-		titular: "Lombi",
-		saldo: 536.2,
+	contaLombi := contas.ContaCorrente {
+		Titular: "Lombi",
+		Saldo: 536.2,
 	}
 
-	fmt.Println(contaLeandro.saldo)
+	fmt.Println(contaLeandro.Saldo)
 	fmt.Println(contaLeandro.Sacar(30))
-	fmt.Println(contaLeandro.saldo)
+	fmt.Println(contaLeandro.Saldo)
 
 	fmt.Println(contaLeandro.Depositar(50))
 
@@ -66,5 +28,5 @@ func main()  {
 
 	resultado := contaLeandro.Transferir(10, &contaLombi)
 	fmt.Println(resultado)
-	fmt.Println(contaLombi)
+	fmt.Println(contaLombi.Saldo)
 }
