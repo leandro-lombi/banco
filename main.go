3c95b5e7fff01121fb6a8ef8c4557761867f3789
leandro.lomb <leandro.lombi@synchro.com.br>
2021-08-05 11:16:34 -0300

    Adicionando interface para aproveitar os metodos comuns em diferentes tipos

--- main.go
+++ main.go
@@ -6,10 +6,22 @@ import (
 	"github.com/lnl/banco/contas"
 )
 
+func PagarBoleto(conta verificarConta, valor float64) {
+	conta.Sacar(valor)
+}
+
+type verificarConta interface {
+	Sacar(valor float64) string
+}
+
 func main() {
 	contaLeandro := contas.ContaCorrente{}
 
 	contaLeandro.Depositar(100)
+
+	fmt.Println(contaLeandro.ObterSaldo())
+
+	PagarBoleto(&contaLeandro, 82)
 
 	fmt.Println(contaLeandro.ObterSaldo())
 
@@ -21,4 +33,7 @@ func main() {
 	contaLombi.Sacar(15)
 	fmt.Println(contaLombi.ObterSaldo())
 
+	PagarBoleto(&contaLombi, 8)
+
+	fmt.Println(contaLombi.ObterSaldo())
 }
